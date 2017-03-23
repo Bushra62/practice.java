35a4fa57dece22b645090d00b1e8730d3672c35c
Bushra62 <Bushranazhnd62@gmail.com>
2017-03-23 12:43:49 -0700

    Create Cube.java

---
+++ Cube.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+import java.util.math;
+public class Cube
+{
+public static void main(String[]args)
+{
+int n,cube;
+Scanner scan=new Scanner(system.in);
+cube=math.pow(n,3);
+System.out.print("Cube is"+cube);
+}
+}
