709f2f829ac16e2e1a9ce28132dde62b81a022ce
Bushra62 <Bushranazhnd62@gmail.com>
2017-03-23 13:37:43 -0700

    Create display.java

---
+++ display.java
@@ -0,0 +1,17 @@
+import.java.Scanner;
+public class Display
+{
+public static void main(String[]args)
+{
+Scanner scan=new Scanner(System.in);
+float a,b,sum,dif,p;
+System.out.println("Enter two number");
+a=scan.nextFloat( )
+b=scan.nextFloat( )
+sum=a+b;
+dif=a-b;
+p=a*b;
+system.out.println("sum is"+sum +"difference is" +dif +"product is" +p);
+}
+}
+
